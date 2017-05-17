5cdc6ddb66b9000ed7888447b22f03c7af989727
Sebastian Rager <mail@interpretor.net>
2017-05-17 16:34:53 +0200

    Ensure compatibility with node v6

--- test/zyre_peer_test.js
+++ test/zyre_peer_test.js
@@ -15,5 +15,25 @@ describe('ZyrePeer', () => {
     };
     zyrePeer.push('foo', 'beacon', obj);
     assert.isDefined(zyrePeer.getPeer('foo'));
+    assert.isDefined(zyrePeer.getPeers().foo);
+  });
+
+  it('should mark an evasive object as evasive', function mocha(done) {
+    // Set higher timeout to test evasive peers
+    this.timeout(6000);
+
+    const zyrePeer = new ZyrePeer();
+    const obj = {
+      address: '1.2.3.4.',
+      port: 12345,
+    };
+    zyrePeer.push('foo', 'beacon', obj);
+
+    zyrePeer.on('evasive', (id, peer) => {
+      assert.equal(id, 'foo');
+      assert.deepEqual(peer.beacon, obj);
+      assert.equal(peer.evasive, true);
+      done();
+    });
   });
 });
