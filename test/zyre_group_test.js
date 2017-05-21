5310c8ddf61cdea6e01e59a5d201b7d1d3ba8439
Sebastian Rager <mail@interpretor.net>
2017-05-21 19:24:32 +0200

    Optimizations and basic zyre_group implementation

--- test/zyre_group_test.js
+++ test/zyre_group_test.js
@@ -3,7 +3,7 @@ const ZyreGroup = require('../lib/zyre_group');
 
 describe('ZyreGroup', () => {
   it('should create an instance of ZyreGroup', () => {
-    const zyreGroup = new ZyreGroup();
+    const zyreGroup = new ZyreGroup('CHAT', { getIdentity() { return 'foo'; } });
     assert.instanceOf(zyreGroup, ZyreGroup);
   });
 });
