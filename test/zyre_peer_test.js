9ac1d31c91f2ed4bd6fdd37b7b55921c13f711e7
Sebastian Rager <mail@interpretor.net>
2017-06-10 21:36:37 +0200

    Increase test coverage

--- test/zyre_peer_test.js
+++ test/zyre_peer_test.js
@@ -8,20 +8,206 @@
 
 const assert = require('chai').assert;
 const uuid = require('uuid');
+const zeromq = require('zeromq');
 const ZyrePeer = require('../lib/zyre_peer');
 
 describe('ZyrePeer', () => {
+  // ZyreGroup mock
+  class Group {
+    constructor(name) {
+      this.name = name;
+      this.peers = {};
+    }
+
+    getName() {
+      return this.name;
+    }
+
+    add(zyrePeer) {
+      if (typeof this.peers[zyrePeer.getIdentity()] === 'undefined') {
+        this.peers[zyrePeer.getIdentity()] = zyrePeer;
+        zyrePeer.addToGroup(this);
+      }
+    }
+
+    remove(zyrePeer) {
+      if (typeof this.peers[zyrePeer.getIdentity()] !== 'undefined') {
+        delete this.peers[zyrePeer.getIdentity()];
+        zyrePeer.removeFromGroup(this);
+      }
+    }
+  }
+
+  let msgHit = 0;
+
+  // ZreMsg mock
+  class Msg {
+    setSequence(sequence) {
+      this.sequence = sequence;
+      assert.equal(this.sequence, 1);
+    }
+
+    send(socket) {
+      msgHit += 1;
+      this.socket = socket;
+      assert.instanceOf(this.socket, zeromq.Socket);
+      return new Promise((resolve) => {
+        resolve();
+      });
+    }
+  }
+
   it('should create an instance of ZyrePeer', () => {
+    const identity = Buffer.alloc(16);
+    uuid.v4(null, identity, 0);
+
     const zyrePeer = new ZyrePeer({
       identity: '12345',
+      originID: identity,
     });
 
     assert.instanceOf(zyrePeer, ZyrePeer);
+    assert.isDefined(zyrePeer._evasive);
+    assert.isDefined(zyrePeer._expired);
+    assert.equal(zyrePeer.getIdentity(), '12345');
+
+    const evasive = 1000;
+    const expired = 2000;
+
+    const zyrePeer2 = new ZyrePeer({
+      identity: '56789',
+      originID: identity,
+      evasive,
+      expired,
+    });
+
+    assert.instanceOf(zyrePeer2, ZyrePeer);
+    assert.equal(zyrePeer2._evasive, evasive);
+    assert.equal(zyrePeer2._expired, expired);
+    assert.equal(zyrePeer2.getIdentity(), '56789');
+  });
+
+  it('should add/remove the ZyrePeer to/from a group', () => {
+    const identity = Buffer.alloc(16);
+    uuid.v4(null, identity, 0);
+
+    const zyrePeer = new ZyrePeer({
+      identity: '12345',
+      originID: identity,
+    });
+
+    const zyreGroup = new Group('CHAT');
+
+    zyrePeer.addToGroup(zyreGroup);
+    assert.property(zyreGroup.peers, '12345');
+    assert.property(zyrePeer._groups, 'CHAT');
+
+    zyrePeer.removeFromGroup(zyreGroup);
+    assert.deepEqual(zyreGroup.peers, {});
+    assert.deepEqual(zyrePeer._groups, {});
+  });
+
+  it('should create a new zeromq dealer socket on connect, pause on disconnect and finally close', () => {
+    const identity = Buffer.alloc(16);
+    uuid.v4(null, identity, 0);
+
+    const zyrePeer = new ZyrePeer({
+      identity: '12345',
+      originID: identity,
+    });
+
+    const zyreGroup = new Group('CHAT');
+    zyrePeer._endpoint = 'tcp://127.0.0.1:42321';
+
+    zyrePeer.addToGroup(zyreGroup);
+    zyrePeer.connect();
+    assert.instanceOf(zyrePeer._socket, zeromq.Socket);
+    zyrePeer.disconnect();
+    assert.isTrue(zyrePeer._socket._paused);
+    zyrePeer.connect();
+    assert.isNotTrue(zyrePeer._socket._paused);
+    zyrePeer.close();
+    assert.deepEqual(zyrePeer._groups, {});
+  });
+
+  it('should send a message to the peer', () => {
+    const identity = Buffer.alloc(16);
+    uuid.v4(null, identity, 0);
+
+    const zyrePeer = new ZyrePeer({
+      identity: '12345',
+      originID: identity,
+    });
+
+    const zreMsg = new Msg();
+    zyrePeer._endpoint = 'tcp://127.0.0.1:42321';
+
+    zyrePeer.connect();
+    zyrePeer.send(zreMsg);
+    assert.equal(msgHit, 1);
+
+    zyrePeer.close();
+  });
+
+  it('should update the peers information', (done) => {
+    const evasive = 100;
+    const expired = 200;
+
+    const identity = Buffer.alloc(16);
+    uuid.v4(null, identity, 0);
+
+    const zyrePeer = new ZyrePeer({
+      identity: '12345',
+      originID: identity,
+      evasive,
+      expired,
+    });
+
+    let hit = false;
+    let hit2 = false;
+
+    zyrePeer.on('disconnect', () => {
+      hit = true;
+    });
+
+    zyrePeer.on('back', () => {
+      hit2 = true;
+    });
+
+    zyrePeer.update({ sequence: 1 });
+    assert.equal(zyrePeer._sequenceIn, 1);
+    assert.isNotObject(zyrePeer.update({ sequence: 3 }));
+    assert.isTrue(hit);
+    hit = false;
+
+    zyrePeer.update({ address: '127.0.0.1', mailbox: 54321 });
+    assert.equal(zyrePeer._endpoint, 'tcp://127.0.0.1:54321');
+    zyrePeer.update({ address: '0.0.0.0' });
+    assert.equal(zyrePeer._endpoint, 'tcp://127.0.0.1:54321');
+    zyrePeer.update({ mailbox: 56123 });
+    assert.equal(zyrePeer._endpoint, 'tcp://127.0.0.1:54321');
+    assert.isNotObject(zyrePeer.update({ address: '0.0.0.0', mailbox: 0 }));
+    assert.isTrue(hit);
+
+    zyrePeer.update({ status: 5 });
+    assert.equal(zyrePeer._status, 5);
+
+    zyrePeer.update({ name: 'foobar' });
+    assert.equal(zyrePeer.getName(), 'foobar');
+
+    zyrePeer.update({ headers: { foo: 'bar' } });
+    assert.deepEqual(zyrePeer._headers, { foo: 'bar' });
+
+    setTimeout(() => {
+      zyrePeer.update({ sequence: 2 });
+      zyrePeer._clearTimeouts();
+      if (hit2) done();
+    }, expired + 50);
   });
 
   it('should mark an evasive peer', (done) => {
-    const evasive = 200;
-    const expired = 400;
+    const evasive = 100;
+    const expired = 200;
 
     const identity = Buffer.alloc(16);
     uuid.v4(null, identity, 0);
@@ -40,12 +226,12 @@ describe('ZyrePeer', () => {
     setTimeout(() => {
       zyrePeer._clearTimeouts();
       if (zyrePeer._evasiveAt > 0) done();
-    }, evasive + 100);
+    }, evasive + 50);
   });
 
   it('should mark an expired peer', (done) => {
-    const evasive = 200;
-    const expired = 400;
+    const evasive = 100;
+    const expired = 200;
 
     const identity = Buffer.alloc(16);
     uuid.v4(null, identity, 0);
@@ -64,5 +250,43 @@ describe('ZyrePeer', () => {
     zyrePeer.on('expired', () => {
       done();
     });
+  });
+
+  it('should return the public peer object', () => {
+    const identity = Buffer.alloc(16);
+    uuid.v4(null, identity, 0);
+
+    const zyrePeer = new ZyrePeer({
+      identity: '12345',
+      originID: identity,
+    });
+
+    const zyreGroup = new Group('CHAT');
+
+    const name = 'foobar';
+    const endpoint = 'tcp://127.0.0.1:54321';
+    const headers = {
+      foo: 'bar',
+      bob: 'omb',
+    };
+
+    zyrePeer.update({
+      name,
+      endpoint,
+      headers,
+    });
+
+    zyrePeer.addToGroup(zyreGroup);
+
+    const obj = zyrePeer.toObj();
+
+    assert.equal(obj.name, name);
+    assert.equal(obj.endpoint, endpoint);
+    assert.deepEqual(obj.headers, headers);
+    assert.sameMembers(obj.groups, ['CHAT']);
+    assert.isNotTrue(obj.evasive);
+    assert.isNotTrue(obj.expired);
+
+    zyrePeer._clearTimeouts();
   });
 });
--- test/zyre_peers_test.js
+++ test/zyre_peers_test.js
@@ -8,9 +8,29 @@
 
 const assert = require('chai').assert;
 const uuid = require('uuid');
+const zeromq = require('zeromq');
 const ZyrePeers = require('../lib/zyre_peers');
 
 describe('ZyrePeers', () => {
+  let msgHit = 0;
+
+  // ZreMsg mock
+  class Msg {
+    setSequence(sequence) {
+      this.sequence = sequence;
+      assert.equal(this.sequence, 1);
+    }
+
+    send(socket) {
+      msgHit += 1;
+      this.socket = socket;
+      assert.instanceOf(this.socket, zeromq.Socket);
+      return new Promise((resolve) => {
+        resolve();
+      });
+    }
+  }
+
   it('should create an instance of ZyrePeers', () => {
     const identity = Buffer.alloc(16);
     uuid.v4(null, identity, 0);
@@ -18,5 +38,109 @@ describe('ZyrePeers', () => {
     const zyrePeers = new ZyrePeers({ identity });
 
     assert.instanceOf(zyrePeers, ZyrePeers);
+  });
+
+  it('should create a new ZyrePeer on push and close it afterwards', () => {
+    const identity = Buffer.alloc(16);
+    uuid.v4(null, identity, 0);
+
+    const zyrePeers = new ZyrePeers({ identity });
+
+    let hit = false;
+
+    zyrePeers.on('new', () => {
+      hit = true;
+    });
+
+    const zyrePeer = zyrePeers.push({
+      identity: '12345',
+      sequence: 1,
+      address: '127.0.0.1',
+      mailbox: 54321,
+    });
+
+    assert.property(zyrePeer, 'connect');
+    assert.property(zyrePeer, 'update');
+    assert.isTrue(hit);
+    assert.isTrue(zyrePeers.exists('12345'));
+    assert.isNotTrue(zyrePeers.exists('56789'));
+    assert.equal(zyrePeers.getPeer('12345'), zyrePeer);
+
+    zyrePeers.close('12345');
+    assert.isNotTrue(zyrePeers.exists('12345'));
+
+    zyrePeers.push({ identity: '123' });
+    zyrePeers.push({ identity: '456' });
+    zyrePeers.closeAll();
+    assert.isNotTrue(zyrePeers.exists('123'));
+    assert.isNotTrue(zyrePeers.exists('456'));
+  });
+
+  it('should send a message to all peers', () => {
+    const identity = Buffer.alloc(16);
+    uuid.v4(null, identity, 0);
+
+    const zyrePeers = new ZyrePeers({ identity });
+    const zreMsg = new Msg();
+
+    const peer1 = zyrePeers.push({ identity: '123', endpoint: 'tcp://127.0.0.1:54678' });
+    const peer2 = zyrePeers.push({ identity: '456', endpoint: 'tcp://127.0.0.1:54679' });
+
+    peer1.connect();
+    peer2.connect();
+    zyrePeers.send(zreMsg);
+    assert.equal(msgHit, 2);
+
+    zyrePeers.closeAll();
+  });
+
+  it('should return the public peers object', () => {
+    const identity = Buffer.alloc(16);
+    uuid.v4(null, identity, 0);
+
+    const zyrePeers = new ZyrePeers({ identity });
+
+    zyrePeers.push({ identity: '123' });
+    zyrePeers.push({ identity: '456' });
+
+    assert.property(zyrePeers.toObj(), '123');
+    assert.property(zyrePeers.toObj(), '456');
+
+    zyrePeers.closeAll();
+  });
+
+  it('should add events to the created peers', (done) => {
+    const evasive = 100;
+    const expired = 200;
+
+    const identity = Buffer.alloc(16);
+    uuid.v4(null, identity, 0);
+
+    const zyrePeers = new ZyrePeers({ identity, evasive, expired });
+
+    let hit1 = false;
+    let hit2 = false;
+    let hit3 = false;
+
+    zyrePeers.on('expired', () => {
+      hit1 = true;
+    });
+
+    zyrePeers.on('back', () => {
+      hit2 = true;
+    });
+
+    zyrePeers.on('disconnect', () => {
+      hit3 = true;
+    });
+
+    zyrePeers.push({ identity: '12345', endpoint: 'tcp://127.0.0.1:56789' });
+
+    setTimeout(() => {
+      zyrePeers.push({ identity: '12345' });
+      zyrePeers.push({ address: '127.0.0.1', mailbox: 0 });
+      zyrePeers.closeAll();
+      if (hit1 && hit2 && hit3) done();
+    }, expired + 50);
   });
 });
