0088d45c7e17b812845ecf6f758fead2f05ac957
Sebastian Rager <mail@interpretor.net>
2017-06-23 23:11:20 +0200

    Increase test coverage

--- test/zyre_node_test.js
+++ test/zyre_node_test.js
@@ -8,25 +8,137 @@
 
 const assert = require('chai').assert;
 const uuid = require('uuid');
-const ZyrePeers = require('../lib/zyre_peers');
-const ZyreGroups = require('../lib/zyre_groups');
 const ZyreNode = require('../lib/zyre_node');
+const ZreMsg = require('../lib/zre_msg');
 const ZHelper = require('../lib/zhelper');
+const ZyreGroups = require('../lib/zyre_groups');
 
 describe('ZyreNode', () => {
+  let msgHit = false;
+
+  // ZyrePeer mock
+  class Peer {
+    constructor() {
+      this.msg = undefined;
+    }
+
+    send(msg) {
+      msgHit = true;
+      this.msg = msg;
+      assert.instanceOf(this.msg, ZreMsg);
+    }
+  }
+
+  // ZyrePeers mock
+  class Peers {
+    constructor() {
+      this.exist = true;
+      this.data = false;
+    }
+
+    exists() {
+      return this.exist;
+    }
+
+    push({ identity, sequence, endpoint, status, name, headers }) {
+      this.data = { identity, sequence, endpoint, status, name, headers };
+      return new Peer();
+    }
+  }
+
   it('should create an instance of ZyreNode', () => {
     const identity = Buffer.alloc(16);
-    uuid.v4(null, this._identity, 0);
+    uuid.v4(null, identity, 0);
 
     const zyreNode = new ZyreNode({
       identity,
       name: 'foo',
       address: ZHelper.getIfData().address,
       mailbox: 54321,
-      zyrePeers: new ZyrePeers(identity),
+      zyrePeers: new Peers(),
       zyreGroups: new ZyreGroups(),
     });
 
     assert.instanceOf(zyreNode, ZyreNode);
+  });
+
+  it('should send a PING_OK message when received a PING', (done) => {
+    const identity = Buffer.alloc(16);
+    uuid.v4(null, identity, 0);
+
+    const zyreNode = new ZyreNode({
+      identity,
+      name: 'foo',
+      address: ZHelper.getIfData().address,
+      mailbox: 54321,
+      zyrePeers: new Peers(),
+      zyreGroups: new ZyreGroups(),
+    });
+
+    const recvID = Buffer.alloc(17);
+    recvID[0] = 1;
+    uuid.v4(null, recvID, 1);
+
+    zyreNode._messageHandler(recvID, new ZreMsg(ZreMsg.PING).toBuffer());
+
+    if (msgHit) {
+      msgHit = false;
+      done();
+    }
+  });
+
+  it('should reject a malformed message', (done) => {
+    const identity = Buffer.alloc(16);
+    uuid.v4(null, identity, 0);
+
+    const zyrePeers = new Peers();
+
+    const zyreNode = new ZyreNode({
+      identity,
+      name: 'foo',
+      address: ZHelper.getIfData().address,
+      mailbox: 54321,
+      zyrePeers,
+      zyreGroups: new ZyreGroups(),
+    });
+
+    const recvID = Buffer.alloc(17);
+    recvID[0] = 1;
+    uuid.v4(null, recvID, 1);
+
+    zyreNode._messageHandler(recvID, 'qwertzuiop1234567890');
+
+    if (!zyrePeers.data) {
+      msgHit = false;
+      done();
+    }
+  });
+
+  it('should reject messages from unknown peers that are not HELLO messages', (done) => {
+    const identity = Buffer.alloc(16);
+    uuid.v4(null, identity, 0);
+
+    const zyrePeers = new Peers();
+    zyrePeers.exist = false;
+
+    const zyreNode = new ZyreNode({
+      identity,
+      name: 'foo',
+      address: ZHelper.getIfData().address,
+      mailbox: 54321,
+      zyrePeers,
+      zyreGroups: new ZyreGroups(),
+    });
+
+    const recvID = Buffer.alloc(17);
+    recvID[0] = 1;
+    uuid.v4(null, recvID, 1);
+
+    zyreNode._messageHandler(recvID, new ZreMsg(ZreMsg.PING).toBuffer());
+
+    if (!zyrePeers.data) {
+      msgHit = false;
+      done();
+    }
   });
 });
